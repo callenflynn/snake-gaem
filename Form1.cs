66c6b30e465ccede640f15784de6f19fce48d3a1
Callen Flynn <not.skittle.or.beluga@gmail.com>
2025-06-28 14:11:36 -0400

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -24,7 +24,7 @@ namespace SnakeGame
             InitializeComponent();
             this.Text = "Cal's Snake Gaem";
             this.DoubleBuffered = true;
-            this.ClientSize = new Size(600, 600); // 50% bigger than 400x400
+            this.ClientSize = new Size(600, 600);
             UpdateCellSize();
             StartGame();
 
